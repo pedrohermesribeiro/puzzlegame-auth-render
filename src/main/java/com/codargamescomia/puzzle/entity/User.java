cae5615efaeb56cc5ff8d3bdf1b8bd0e41febaad
Pedro Ribeiro <pedrohermesrib@gmail.com>
2026-04-11 20:11:46 -0300

    renomeando o nome da tabela user

--- src/main/java/com/codargamescomia/puzzle/entity/User.java
+++ src/main/java/com/codargamescomia/puzzle/entity/User.java
@@ -6,13 +6,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-@Table(name = "`user`")
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
